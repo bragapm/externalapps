ce67fd74b22139484ce0ec328ed5777d1b3c193f
Miko <jherjati@gmail.com>
2024-01-09 00:43:10 +0700

    App : update deploy flow on local sh script and cicd

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 # Start build processes in the background and store their PIDs
-(cd directus/extensions && pnpm build) &
+(cd extensions && pnpm build) &
 PID1=$!
-(cd vite && pnpm build) &
-PID2=$!
 (cd web && pnpm build) &
-PID3=$!
+PID2=$!
 
 # Wait for all background processes to finish and capture their exit statuses
 wait $PID1
 STATUS1=$?
 wait $PID2
 STATUS2=$?
-wait $PID3
-STATUS3=$?
 
 # Check if all builds were successful
-if [ $STATUS1 -eq 0 ] && [ $STATUS2 -eq 0 ] && [ $STATUS3 -eq 0 ]; then
+if [ $STATUS1 -eq 0 ] && [ $STATUS2 -eq 0 ]; then
     echo "All builds succeeded. Proceeding with deployment."
     eb deploy
     echo "Build and deploy completed."
