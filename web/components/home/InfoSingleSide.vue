a8fe5b413f99d3a2c2fe848f1395ec6f47706983
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-01-16 19:37:15 +0700

    Web: Add singular info blocks. Delete placeholder

---
+++ web/components/home/InfoSingleSide.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { IBlockInfoSingleItem } from "./InfoSingleBottom.vue";
+
+defineProps<{
+  item: IBlockInfoSingleItem;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-3 bg-black/5 rounded-lg p-11">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-x-7">
+      <div class="flex flex-col justify-between space-y-3">
+        <div class="space-y-3">
+          <p>{{ item.subtitle }}</p>
+          <h1 class="text-4xl">{{ item.title }}</h1>
+        </div>
+        <div class="space-y-3">
+          <p>{{ item.body }}</p>
+          <UButton
+            v-if="item.button_text && item.button_url"
+            color="black"
+            :ui="{ rounded: 'rounded-[4px]' }"
+            class="p-3"
+            :to="item.button_url"
+            target="_blank"
+          >
+            {{ item.button_text }}
+          </UButton>
+        </div>
+      </div>
+      <NuxtImg
+        v-if="item.image"
+        provider="directus"
+        :src="item.image"
+        class="w-full h-[28rem] mt-3 rounded-lg object-cover"
+      />
+    </div>
+  </div>
+</template>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { IBlockHeroItem } from "~/components/home/Hero.vue";
 import type { IBlockHeroSingleItem } from "~/components/home/HeroSingle.vue";
+import type { IBlockInfoSingleItem } from "~/components/home/InfoSingleBottom.vue";
 
 interface IHomeBlocks {
   id: number;
@@ -13,15 +14,19 @@ interface IBlockHeroSingle extends IHomeBlocks {
   collection: "block_hero_single";
   item: IBlockHeroSingleItem;
 }
+interface IBlockInfoSingle extends IHomeBlocks {
+  collection: "block_info_single";
+  item: IBlockInfoSingleItem;
+}
 
 interface IHomeData {
   data: {
-    blocks: (IBlockHero | IBlockHeroSingle)[];
+    blocks: (IBlockHero | IBlockHeroSingle | IBlockInfoSingle)[];
   };
 }
 
 const { data: homeData, error: homeDataError } = await useFetch<IHomeData>(
-  `/panel/items/home/eng?fields=blocks.id,blocks.collection,blocks.item:block_hero.slides.id,blocks.item:block_hero.slides.block_hero_slides_id.*,blocks.item:block_hero_single.*`
+  `/panel/items/home/eng?fields=blocks.id,blocks.collection,blocks.item:block_hero.slides.id,blocks.item:block_hero.slides.block_hero_slides_id.*,blocks.item:block_hero_single.*,blocks.item:block_info_single.*`
 );
 </script>
 
@@ -34,10 +39,17 @@ const { data: homeData, error: homeDataError } = await useFetch<IHomeData>(
     >
       <HomeHero v-if="block.collection === 'block_hero'" :item="block.item" />
       <HomeHeroSingle
-        v-if="block.collection === 'block_hero_single'"
+        v-else-if="block.collection === 'block_hero_single'"
+        :item="block.item"
+      />
+      <HomeInfoSingleBottom
+        v-else-if="block.collection === 'block_info_single' && block.item.variant === 'bottom'"
+        :item="block.item"
+      />
+      <HomeInfoSingleSide
+        v-else-if="block.collection === 'block_info_single' && block.item.variant === 'side'"
         :item="block.item"
       />
     </div>
-    <HomeSection1 />
   </div>
 </template>
