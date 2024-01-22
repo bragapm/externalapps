08c6fcb96896ce5cf86a06cf1a0296e720ebbf7b
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-01-22 14:49:05 +0700

    Web: Add InfoAccordion

---
+++ web/components/home/InfoAccordion.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import IcArrowReg from "~/assets/icons/ic-arrow-reg.svg";
+
+export interface IBlockInfoAccordionItem {
+  id: number;
+  title: string;
+  subtitle: string;
+  body: string;
+  contents: {
+    id: number;
+    block_info_accordion_contents_id: {
+      id: number;
+      headline: string;
+      body: string;
+      image: string;
+    };
+  }[];
+}
+
+const props = defineProps<{
+  item: IBlockInfoAccordionItem;
+}>();
+
+const accordionStates = ref(
+  props.item.contents.map((_el, index) => index === 0)
+);
+const accordionHandler = (index: number) => {
+  accordionStates.value.forEach((_el, i) => {
+    if (i === index) {
+      accordionStates.value[i] = !accordionStates.value[i];
+    } else {
+      accordionStates.value[i] = false;
+    }
+  });
+};
+</script>
+
+<template>
+  <div class="flex flex-col gap-3 bg-black/5 rounded-lg p-11 min-h-[42rem]">
+    <p>{{ item.subtitle }}</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-x-7">
+      <div class="space-y-3">
+        <h1 class="text-4xl">{{ item.title }}</h1>
+        <p>{{ item.body }}</p>
+      </div>
+      <div class="flex flex-col">
+        <div v-for="(content, index) of item.contents" :key="content.id">
+          <button
+            :class="[
+              'flex justify-between p-4 w-full text-xl font-medium rounded-t-lg',
+              accordionStates[index] && 'bg-black text-white',
+            ]"
+            @click="accordionHandler(index)"
+          >
+            {{ content.block_info_accordion_contents_id.headline }}
+            <IcArrowReg :class="!accordionStates[index] && 'rotate-180'" />
+          </button>
+          <div
+            v-if="accordionStates[index]"
+            class="bg-black text-white rounded-b-lg overflow-hidden"
+          >
+            <div class="space-y-3 p-4">
+              <NuxtImg
+                provider="directus"
+                :src="content.block_info_accordion_contents_id.image"
+                class="w-full h-48 object-cover"
+              />
+              <p>{{ content.block_info_accordion_contents_id.body }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
