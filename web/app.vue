ca7b6bb8bde6f404eab1a6544f6e0092b56b4308
Miko <jherjati@gmail.com>
2024-01-29 13:25:15 +0700

    Web : add useSeoMeta and dynamic favicon

--- web/app.vue
+++ web/app.vue
@@ -1,4 +1,32 @@
+<script setup lang="ts">
+const { data, pending } = useFetch<{
+  data: {
+    help_center_url?: string;
+    project_descriptor?: string;
+    project_logo_horizontal?: string;
+    project_name: string;
+    public_favicon?: string;
+  };
+}>("/panel/settings");
+
+useSeoMeta({
+  title: data?.value?.data.project_name || "",
+  ogTitle: data?.value?.data.project_name || "",
+  description: data?.value?.data.project_descriptor || "",
+  ogDescription: data?.value?.data.project_descriptor || "",
+  ogImage: data?.value?.data.public_favicon
+    ? `/panel/assets/${data.value.data.public_favicon}`
+    : "",
+});
+</script>
+
 <template>
+  <Head>
+    <Link
+      rel="icon"
+      type="image/png"
+      :href="`/panel/assets/${data?.data.public_favicon}`"
+  /></Head>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
--- web/utils/index.ts
+++ web/utils/index.ts
@@ -4,6 +4,5 @@ export const useMapData = async () => {
   const { pending, data } = await useFetch<MapData>("/panel/items/map/eng", {
     key: "map",
   });
-  console.log({ mapData: data });
   return { data, isLoading: pending };
 };
