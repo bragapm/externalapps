bccb90202b953ddfd34fa055f974cd35a6e12c4a
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-02-02 16:58:55 +0700

    Extensions: Make constraints creation integrated in create table + use PUBLIC_FOLDER_ID const in map page structure migrations

--- extensions/extensions/migrations/20240112GD1-map-page-structure-and-permissions.mjs
+++ extensions/extensions/migrations/20240112GD1-map-page-structure-and-permissions.mjs
@@ -1,17 +1,16 @@
+import { PUBLIC_FOLDER_ID } from "./const/FOLDER_IDS.mjs";
+
 export async function up(knex) {
   await knex.raw(`
     CREATE TABLE IF NOT EXISTS public."map" (
-      lang varchar(255) NOT NULL,
-      user_updated uuid NULL,
-      date_updated timestamptz NULL,
-      information text NULL,
-      title varchar(255) NULL,
-      subtitle varchar(255) NULL,
-      initial_map_view json NULL,
-      CONSTRAINT map_pkey PRIMARY KEY (lang)
+      lang varchar(255) NOT NULL PRIMARY KEY,
+      user_updated uuid REFERENCES directus_users (id),
+      date_updated timestamptz,
+      information text,
+      title varchar(255),
+      subtitle varchar(255),
+      initial_map_view json
     );
-
-    ALTER TABLE public."map" ADD CONSTRAINT map_user_updated_foreign FOREIGN KEY (user_updated) REFERENCES public.directus_users(id);
 
     INSERT INTO directus_collections(collection,icon,hidden,color,"group")
     VALUES ('map','map',FALSE,'#3399FF','pages');
@@ -21,7 +20,7 @@ export async function up(knex) {
       ('map','lang',NULL,'input',NULL,NULL,NULL,false,false,1,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
       ('map','user_updated','user-updated','select-dropdown-m2o','{"template":"{{avatar.$thumbnail}} {{first_name}} {{last_name}}"}','user',NULL,true,true,2,'half',NULL,NULL,NULL,false,NULL,NULL,NULL),
       ('map','date_updated','date-updated','datetime',NULL,'datetime','{"relative":true}',true,true,3,'half',NULL,NULL,NULL,false,NULL,NULL,NULL),
-      ('map','information',NULL,'input-rich-text-md','{"folder":"ffffffff-ffff-4fff-bfff-ffffffffffff"}',NULL,NULL,false,false,4,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
+      ('map','information',NULL,'input-rich-text-md','{"folder":"${PUBLIC_FOLDER_ID}"}',NULL,NULL,false,false,4,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
       ('map','title',NULL,'input',NULL,NULL,NULL,false,false,5,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
       ('map','initial_map_view','cast-json','map','{"defaultView":{"center":{"lng":118.10483551162508,"lat":-0.9964696394390842},"zoom":3.356805567596401,"bearing":0,"pitch":0},"geometryType":"Point"}',NULL,NULL,false,false,7,'full',NULL,NULL,NULL,false,NULL,NULL,NULL);
 
