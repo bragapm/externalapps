d35edd7e97c9fde72c39b6cf1f60eaca822e7ee3
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-02-13 19:04:46 +0700

    Build web in container

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,17 +3,13 @@
 # Start build processes in the background and store their PIDs
 (cd extensions && pnpm build) &
 PID1=$!
-(cd web && pnpm build) &
-PID2=$!
 
 # Wait for all background processes to finish and capture their exit statuses
 wait $PID1
 STATUS1=$?
-wait $PID2
-STATUS2=$?
 
 # Check if all builds were successful
-if [ $STATUS1 -eq 0 ] && [ $STATUS2 -eq 0 ]; then
+if [ $STATUS1 -eq 0 ]; then
     echo "All builds succeeded. Proceeding with deployment."
     eb deploy
     echo "Build and deploy completed."
