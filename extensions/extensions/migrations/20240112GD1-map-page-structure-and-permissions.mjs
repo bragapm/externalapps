0e62e3d841bf199616a8d61f9058ebc5f6278598
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-02-15 21:39:43 +0700

    Extensions: Add information_attachments in map page structure migration

--- extensions/extensions/migrations/20240112GD1-map-page-structure-and-permissions.mjs
+++ extensions/extensions/migrations/20240112GD1-map-page-structure-and-permissions.mjs
@@ -7,6 +7,7 @@ export async function up(knex) {
       user_updated uuid REFERENCES directus_users (id),
       date_updated timestamptz,
       information text,
+      information_attachments json,
       title varchar(255),
       subtitle varchar(255),
       initial_map_view json
@@ -16,15 +17,16 @@ export async function up(knex) {
     VALUES ('map','map',FALSE,'#3399FF','pages');
 
     INSERT INTO public.directus_fields (collection,field,special,interface,"options",display,display_options,readonly,hidden,sort,width,translations,note,conditions,required,"group",validation,validation_message) 
-    VALUES ('map','subtitle',NULL,'input',NULL,NULL,NULL,false,false,6,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
-      ('map','lang',NULL,'input',NULL,NULL,NULL,false,false,1,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
-      ('map','user_updated','user-updated','select-dropdown-m2o','{"template":"{{avatar.$thumbnail}} {{first_name}} {{last_name}}"}','user',NULL,true,true,2,'half',NULL,NULL,NULL,false,NULL,NULL,NULL),
-      ('map','date_updated','date-updated','datetime',NULL,'datetime','{"relative":true}',true,true,3,'half',NULL,NULL,NULL,false,NULL,NULL,NULL),
-      ('map','information',NULL,'input-rich-text-md','{"folder":"${PUBLIC_FOLDER_ID}"}',NULL,NULL,false,false,4,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
-      ('map','title',NULL,'input',NULL,NULL,NULL,false,false,5,'full',NULL,NULL,NULL,false,NULL,NULL,NULL),
-      ('map','initial_map_view','cast-json','map','{"defaultView":{"center":{"lng":118.10483551162508,"lat":-0.9964696394390842},"zoom":3.356805567596401,"bearing":0,"pitch":0},"geometryType":"Point"}',NULL,NULL,false,false,7,'full',NULL,NULL,NULL,false,NULL,NULL,NULL);
+    VALUES ('map','subtitle',NULL,'input',NULL,NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('map','lang',NULL,'input',NULL,NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('map','user_updated','user-updated','select-dropdown-m2o','{"template":"{{avatar.$thumbnail}} {{first_name}} {{last_name}}"}','user',NULL,TRUE,TRUE,NULL,'half',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('map','date_updated','date-updated','datetime',NULL,'datetime','{"relative":true}',TRUE,TRUE,NULL,'half',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('map','information',NULL,'input-rich-text-md','{"folder":"${PUBLIC_FOLDER_ID}"}',NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('map','information_attachments','cast-json','list','{"fields":[{"field":"title","name":"title","type":"string","meta":{"field":"title","width":"half","type":"string","required":true,"interface":"input"}},{"field":"description","name":"description","type":"string","meta":{"field":"description","width":"half","type":"string","required":true,"interface":"input"}},{"field":"url","name":"url","type":"string","meta":{"field":"url","width":"half","type":"string","required":true,"interface":"input"}},{"field":"icon","name":"icon","type":"string","meta":{"field":"icon","width":"half","type":"string","required":true,"interface":"select-dropdown","options":{"choices":[{"text":"link","value":"link"},{"text":"form","value":"form"}]}}}]}',NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('map','title',NULL,'input',NULL,NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('map','initial_map_view','cast-json','map','{"defaultView":{"center":{"lng":118.10483551162508,"lat":-0.9964696394390842},"zoom":3.356805567596401,"bearing":0,"pitch":0},"geometryType":"Point"}',NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL);
 
-    INSERT INTO directus_permissions(collection,action,fields) VALUES ('map','read','lang,information,title,initial_map_view');
+    INSERT INTO directus_permissions(collection,action,fields) VALUES ('map','read','lang,information,information_attachments,title,subtitle,initial_map_view');
   `);
 }
 
