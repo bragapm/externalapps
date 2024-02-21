bcc5473729f629a2fa6398260f329d48b4c6835b
Miko <jherjati@gmail.com>
2024-02-21 13:27:53 +0700

    Popup : Refactor highlight, remove slider bug, change color. Directus : Apply 5m CACHE_SYSTEM_TTL

--- web/components/map/Popup.vue
+++ web/components/map/Popup.vue
@@ -3,7 +3,6 @@ import type {
   GeoJSONSource,
   LngLatBoundsLike,
   LngLatLike,
-  Map,
   MapGeoJSONFeature,
   MapMouseEvent,
   PointLike,
@@ -12,142 +11,16 @@ import { ref } from "vue";
 import maplibregl from "maplibre-gl";
 import IcArrowReg from "~/assets/icons/ic-arrow-reg.svg";
 import IcCross from "~/assets/icons/ic-cross.svg";
-import KeenSlider, {
-  type KeenSliderInstance,
-  // type KeenSliderPlugin,
-} from "keen-slider";
-import type { Raw } from "vue";
-import tailwindConfig from "~/tailwind.config";
+import KeenSlider, { type KeenSliderInstance } from "keen-slider";
 import bbox from "@turf/bbox";
+import { showHighlightLayer } from "~/utils/index";
 
-/* const ResizePlugin: KeenSliderPlugin = (slider) => {
-  const observer = new ResizeObserver(function () {
-    slider.update();
-  });
+const mapRefStore = useMapRef();
+const featureStore = useFeature();
+const { map } = storeToRefs(mapRefStore);
+const mapLayerStore = useMapLayer();
 
-  slider.on("created", () => {
-    observer.observe(slider.container);
-  });
-  slider.on("destroyed", () => {
-    observer.unobserve(slider.container);
-  });
-}; */
-
-const showHighlightLayer = (
-  map: Raw<Map>,
-  featureList: { geom: GeoJSON.Geometry }[]
-) => {
-  const newData: GeoJSON.FeatureCollection = {
-    type: "FeatureCollection",
-    features: featureList
-      .filter((_, idx) => idx === 0)
-      .map(({ geom, ...rest }) => ({
-        type: "Feature",
-        properties: rest,
-        geometry: geom,
-      })),
-  };
-  if (featureList[0].geom.type !== "Point") {
-    map?.fitBounds(bbox(newData) as LngLatBoundsLike, { padding: 60 });
-  } else {
-    map?.flyTo({
-      zoom: 15,
-      center: featureList[0].geom.coordinates as LngLatLike,
-    });
-  }
-  if (map.getSource("highlight")) {
-    (map.getSource("highlight") as GeoJSONSource).setData(newData);
-  } else {
-    map.addSource("highlight", {
-      type: "geojson",
-      data: newData,
-    });
-
-    map.addLayer({
-      id: "pulsing-dot",
-      type: "symbol",
-      source: "highlight",
-      layout: {
-        "icon-image": "pulsing-dot",
-      },
-      filter: ["==", "$type", "Point"],
-    });
-
-    // add a line layer without line-dasharray defined to fill the gaps in the dashed line
-    map.addLayer({
-      type: "line",
-      source: "highlight",
-      id: "line-background",
-      layout: { "line-cap": "round", "line-join": "round" },
-      paint: {
-        "line-color": (tailwindConfig.theme?.colors as any).grey["600"],
-        "line-width": 6,
-        "line-opacity": 0.3,
-      },
-      filter: ["!=", "$type", "Point"],
-    });
-
-    // add a line layer with line-dasharray set to the first value in dashArraySequence
-    map.addLayer({
-      type: "line",
-      source: "highlight",
-      id: "line-dashed",
-      layout: { "line-join": "round" },
-      paint: {
-        "line-color": (tailwindConfig.theme?.colors as any).grey["300"],
-        "line-width": 6,
-        "line-opacity": 0.6,
-        "line-dasharray": [0, 4, 3],
-      },
-      filter: ["!=", "$type", "Point"],
-    });
-
-    // technique based on https://jsfiddle.net/2mws8y3q/
-    // an array of valid line-dasharray values, specifying the lengths of the alternating dashes and gaps that form the dash pattern
-    const dashArraySequence = [
-      [0, 4, 3],
-      [0.5, 4, 2.5],
-      [1, 4, 2],
-      [1.5, 4, 1.5],
-      [2, 4, 1],
-      [2.5, 4, 0.5],
-      [3, 4, 0],
-      [0, 0.5, 3, 3.5],
-      [0, 1, 3, 3],
-      [0, 1.5, 3, 2.5],
-      [0, 2, 3, 2],
-      [0, 2.5, 3, 1.5],
-      [0, 3, 3, 1],
-      [0, 3.5, 3, 0.5],
-    ];
-
-    let step = 0;
-
-    function animateDashArray(timestamp: DOMHighResTimeStamp) {
-      // Update line-dasharray using the next value in dashArraySequence. The
-      // divisor in the expression `timestamp / 50` controls the animation speed.
-      const newStep = parseInt(
-        ((timestamp / 60) % dashArraySequence.length).toString()
-      );
-
-      if (newStep !== step) {
-        map.setPaintProperty(
-          "line-dashed",
-          "line-dasharray",
-          dashArraySequence[step]
-        );
-        step = newStep;
-      }
-
-      // Request the next frame of the animation.
-      requestAnimationFrame(animateDashArray);
-    }
-
-    // start the animation
-    animateDashArray(0);
-  }
-};
-
+// Slider Logic
 const sliderContainer = ref<HTMLElement | null>(null);
 let slider: KeenSliderInstance | null = null;
 let nextImage: (e: MouseEvent) => void;
@@ -174,10 +47,6 @@ onMounted(() => {
   }
 });
 
-onUnmounted(() => {
-  slider?.destroy();
-});
-
 // Popup Logic
 export type PopupItem = {
   layerType: string;
@@ -189,21 +58,16 @@ export type PopupItem = {
   geometry: GeoJSON.Geometry;
 };
 
-const mapRefStore = useMapRef();
-const featureStore = useFeature();
-const { map } = storeToRefs(mapRefStore);
-const mapLayerStore = useMapLayer();
-
 const contentRef = ref<HTMLDivElement>();
 const popupItems = ref<PopupItem[]>([]);
 const popupRef = ref<maplibregl.Popup>();
 const features = ref<any[]>([]);
 const isFetching = ref(false);
+const featureIndex = ref(0);
 
 watchEffect(() => {
   if (!map.value) return;
   map.value.on("click", (e: MapMouseEvent & Object) => {
-    itemIndex.value = 0;
     // The 'point' to query for features
     const point = [e.point.x, e.point.y];
     const filterLayers = mapLayerStore.groupedActiveLayers
@@ -246,21 +110,15 @@ watchEffect(() => {
         .setMaxWidth("400px")
         .setDOMContent(contentRef.value)
         .addTo(map.value!);
-      window.dispatchEvent(new Event("resize"));
       slider?.moveToIdx(0);
     }
   });
 });
 
 onUnmounted(() => {
-  if (popupRef.value) {
-    popupRef.value.remove();
-  }
+  slider?.destroy();
+  popupRef.value?.remove();
 });
-
-const handleClose = () => {
-  popupRef.value!.remove();
-};
 
 watchEffect(async () => {
   if (popupItems.value?.length) {
@@ -287,18 +145,19 @@ watchEffect(async () => {
     );
     isFetching.value = false;
     features.value = data;
+    featureIndex.value = 0;
+    slider?.update();
     showHighlightLayer(map.value!, data as any[]);
   }
 });
 
-const itemIndex = ref(0);
-const nextIndex = () => {
-  if (itemIndex.value === popupItems.value.length - 1) return;
-  itemIndex.value++;
+const nextFeature = () => {
+  if (featureIndex.value === popupItems.value.length - 1) return;
+  featureIndex.value++;
   const newData: GeoJSON.FeatureCollection = {
     type: "FeatureCollection",
     features: features.value
-      .filter((_, idx) => idx === itemIndex.value)
+      .filter((_, idx) => idx === featureIndex.value)
       .map(({ geom, ...rest }) => ({
         type: "Feature",
         properties: rest,
@@ -306,23 +165,25 @@ const nextIndex = () => {
       })),
   };
   (map.value!.getSource("highlight") as GeoJSONSource).setData(newData);
-  if (features.value[itemIndex.value].geom.type !== "Point") {
+  if (features.value[featureIndex.value].geom.type !== "Point") {
     map.value?.fitBounds(bbox(newData) as LngLatBoundsLike, { padding: 60 });
   } else {
     map.value?.flyTo({
       zoom: 15,
-      center: features.value[itemIndex.value].geom.coordinates as LngLatLike,
+      center: features.value[featureIndex.value].geom.coordinates as LngLatLike,
     });
   }
   slider?.moveToIdx(0);
+  slider?.update();
 };
-const prevIndex = () => {
-  if (itemIndex.value === 0) return;
-  itemIndex.value--;
+
+const prevFeature = () => {
+  if (featureIndex.value === 0) return;
+  featureIndex.value--;
   const newData: GeoJSON.FeatureCollection = {
     type: "FeatureCollection",
     features: features.value
-      .filter((_, idx) => idx === itemIndex.value)
+      .filter((_, idx) => idx === featureIndex.value)
       .map(({ geom, ...rest }) => ({
         type: "Feature",
         properties: rest,
@@ -330,15 +191,16 @@ const prevIndex = () => {
       })),
   };
   (map.value!.getSource("highlight") as GeoJSONSource).setData(newData);
-  if (features.value[itemIndex.value].geom.type !== "Point") {
+  if (features.value[featureIndex.value].geom.type !== "Point") {
     map.value?.fitBounds(bbox(newData) as LngLatBoundsLike, { padding: 60 });
   } else {
     map.value?.flyTo({
       zoom: 15,
-      center: features.value[itemIndex.value].geom.coordinates as LngLatLike,
+      center: features.value[featureIndex.value].geom.coordinates as LngLatLike,
     });
   }
   slider?.moveToIdx(0);
+  slider?.update();
 };
 </script>
 
@@ -362,8 +224,7 @@ const prevIndex = () => {
                   type: 'FeatureCollection',
                   features: []
                 } as any );
-                handleClose();
-              }
+                popupRef!.remove();              }
             "
             class="w-3 h-3 text-grey-400"
           ></IcCross>
@@ -371,7 +232,7 @@ const prevIndex = () => {
 
         <div
           :class="`relative w-full ${
-            popupItems[itemIndex]?.imageColumns?.length ? 'h-40' : 'h-0'
+            popupItems[featureIndex]?.imageColumns?.length ? 'h-40' : 'h-0'
           }`"
         >
           <div
@@ -380,12 +241,14 @@ const prevIndex = () => {
           >
             <img
               class="keen-slider__slide object-cover min-w-full max-w-full"
-              v-for="(val, idx) of Object.keys(features[itemIndex] ?? {})
-                .filter((k) => popupItems[itemIndex].imageColumns?.includes(k))
+              v-for="(val, idx) of Object.keys(features[featureIndex] ?? {})
+                .filter((k) =>
+                  popupItems[featureIndex]?.imageColumns?.includes(k)
+                )
                 .map((k) =>
-                  features[itemIndex][k].includes(',')
-                    ? features[itemIndex][k].split(',')
-                    : features[itemIndex][k]
+                  features[featureIndex][k].includes(',')
+                    ? features[featureIndex][k].split(',')
+                    : features[featureIndex][k]
                 )
                 .flat()"
               :key="idx"
@@ -395,8 +258,8 @@ const prevIndex = () => {
 
           <button
             v-if="
-              Object.keys(features[itemIndex] ?? {}).filter((k) =>
-                popupItems[itemIndex].imageColumns?.includes(k)
+              Object.keys(features[featureIndex] ?? {}).filter((k) =>
+                popupItems[featureIndex].imageColumns?.includes(k)
               ).length
             "
             @click="prevImage"
@@ -410,8 +273,8 @@ const prevIndex = () => {
 
           <button
             v-if="
-              Object.keys(features[itemIndex] ?? {}).filter((k) =>
-                popupItems[itemIndex].imageColumns?.includes(k)
+              Object.keys(features[featureIndex] ?? {}).filter((k) =>
+                popupItems[featureIndex].imageColumns?.includes(k)
               ).length
             "
             @click="nextImage"
@@ -431,11 +294,11 @@ const prevIndex = () => {
           <h5 class="text-xs font-medium">Layer</h5>
           <div class="flex text-grey-400 space-x-2">
             <p class="text-2xs w-1/4">Name</p>
-            <p class="text-xs">: {{ popupItems[itemIndex].tableName }}</p>
+            <p class="text-xs">: {{ popupItems[featureIndex].tableName }}</p>
           </div>
           <div class="flex text-grey-400 space-x-2">
             <p class="text-2xs w-1/4">Type</p>
-            <p class="text-xs">: {{ popupItems[itemIndex].layerType }}</p>
+            <p class="text-xs">: {{ popupItems[featureIndex].layerType }}</p>
           </div>
 
           <h5 class="text-xs font-medium">Feature</h5>
@@ -451,14 +314,14 @@ const prevIndex = () => {
           </template>
           <template v-else>
             <div
-              v-for="key of Object.keys(features[itemIndex] ?? {}).filter((k) =>
-                popupItems[itemIndex].clickPopupColumns?.includes(k)
+              v-for="key of Object.keys(features[featureIndex] ?? {}).filter(
+                (k) => popupItems[featureIndex].clickPopupColumns?.includes(k)
               )"
               :key="key"
               class="flex text-grey-400 space-x-2"
             >
               <p class="text-2xs w-1/4">{{ key }}</p>
-              <p class="text-xs">: {{ features[itemIndex][key] }}</p>
+              <p class="text-xs">: {{ features[featureIndex][key] }}</p>
             </div>
           </template>
         </article>
@@ -468,16 +331,15 @@ const prevIndex = () => {
             role="button"
             :fontControlled="false"
             :disabled="popupItems?.length < 2"
-            @click="prevIndex"
+            @click="prevFeature"
             class="rounded-xs border w-9 h-9 flex justify-center items-center -rotate-90 text-grey-400 border-grey-400"
           />
           <button
             @click="
               () => {
-                featureStore.setFeature(popupItems[itemIndex]);
+                featureStore.setFeature(popupItems[featureIndex]);
                 featureStore.setRightSidebar('feature');
-                handleClose();
-              }
+                popupRef!.remove()             }
             "
             class="rounded-xs grow h-9 bg-brand-600 text-sm font-medium"
           >
@@ -487,7 +349,7 @@ const prevIndex = () => {
             role="button"
             :fontControlled="false"
             :disabled="popupItems?.length < 2"
-            @click="nextIndex"
+            @click="nextFeature"
             class="rounded-xs border w-9 h-9 flex justify-center items-center rotate-90 text-grey-400 border-grey-400"
           />
         </footer>
--- web/utils/index.ts
+++ web/utils/index.ts
@@ -1,5 +1,13 @@
-import type { Map } from "maplibre-gl";
+import type {
+  GeoJSONSource,
+  LngLatBoundsLike,
+  LngLatLike,
+  Map,
+} from "maplibre-gl";
 import type { MapData } from "./types";
+import type { Raw } from "vue";
+import bbox from "@turf/bbox";
+import tailwindConfig from "~/tailwind.config";
 
 export const useMapData = async () => {
   const { pending, data } = await useFetch<MapData>("/panel/items/map/eng", {
@@ -8,8 +16,124 @@ export const useMapData = async () => {
   return { data, isLoading: pending };
 };
 
-// This implements `StyleImageInterface`
-// to draw a pulsing dot icon on the map.
+export const showHighlightLayer = (
+  map: Raw<Map>,
+  featureList: { geom: GeoJSON.Geometry }[]
+) => {
+  const newData: GeoJSON.FeatureCollection = {
+    type: "FeatureCollection",
+    features: featureList
+      .filter((_, idx) => idx === 0)
+      .map(({ geom, ...rest }) => ({
+        type: "Feature",
+        properties: rest,
+        geometry: geom,
+      })),
+  };
+  console.log(featureList[0].geom.type);
+
+  if (featureList[0].geom.type !== "Point") {
+    map?.fitBounds(bbox(newData) as LngLatBoundsLike, { padding: 60 });
+  } else {
+    map?.flyTo({
+      zoom: 15,
+      center: featureList[0].geom.coordinates as LngLatLike,
+    });
+  }
+  if (map.getSource("highlight")) {
+    (map.getSource("highlight") as GeoJSONSource).setData(newData);
+  } else {
+    map.addSource("highlight", {
+      type: "geojson",
+      data: newData,
+    });
+
+    map.addLayer({
+      id: "pulsing-dot",
+      type: "symbol",
+      source: "highlight",
+      layout: {
+        "icon-image": "pulsing-dot",
+      },
+      filter: ["==", "$type", "Point"],
+    });
+
+    // add a line layer without line-dasharray defined to fill the gaps in the dashed line
+    map.addLayer({
+      type: "line",
+      source: "highlight",
+      id: "line-background",
+      layout: { "line-cap": "round", "line-join": "round" },
+      paint: {
+        "line-color": (tailwindConfig.theme?.colors as any).brand["500"],
+        "line-width": 6,
+        "line-opacity": 0.9,
+      },
+      filter: ["!=", "$type", "Point"],
+      // filter: ["==", "$type", "LineString"],
+    });
+
+    // add a line layer with line-dasharray set to the first value in dashArraySequence
+    map.addLayer({
+      type: "line",
+      source: "highlight",
+      id: "line-dashed",
+      layout: { "line-join": "round" },
+      paint: {
+        "line-color": (tailwindConfig.theme?.colors as any).brand["50"],
+        "line-width": 6,
+        "line-dasharray": [0, 4, 3],
+      },
+      filter: ["!=", "$type", "Point"],
+      // filter: ["==", "$type", "LineString"],
+    });
+
+    // technique based on https://jsfiddle.net/2mws8y3q/
+    // an array of valid line-dasharray values, specifying the lengths of the alternating dashes and gaps that form the dash pattern
+    const dashArraySequence = [
+      [0, 4, 3],
+      [0.5, 4, 2.5],
+      [1, 4, 2],
+      [1.5, 4, 1.5],
+      [2, 4, 1],
+      [2.5, 4, 0.5],
+      [3, 4, 0],
+      [0, 0.5, 3, 3.5],
+      [0, 1, 3, 3],
+      [0, 1.5, 3, 2.5],
+      [0, 2, 3, 2],
+      [0, 2.5, 3, 1.5],
+      [0, 3, 3, 1],
+      [0, 3.5, 3, 0.5],
+    ];
+
+    let step = 0;
+
+    function animateDashArray(timestamp: DOMHighResTimeStamp) {
+      // Update line-dasharray using the next value in dashArraySequence. The
+      // divisor in the expression `timestamp / 50` controls the animation speed.
+      const newStep = parseInt(
+        ((timestamp / 60) % dashArraySequence.length).toString()
+      );
+
+      if (newStep !== step) {
+        map.setPaintProperty(
+          "line-dashed",
+          "line-dasharray",
+          dashArraySequence[step]
+        );
+        step = newStep;
+      }
+
+      // Request the next frame of the animation.
+      requestAnimationFrame(animateDashArray);
+    }
+
+    // start the animation
+    animateDashArray(0);
+  }
+};
+
 export const createPulsingDot = (map: Map, size: number) => ({
   width: size,
   height: size,
