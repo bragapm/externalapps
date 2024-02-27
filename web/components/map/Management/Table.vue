784e93baa5c8c08a8431cff1ec0d181cb2882cb0
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2024-02-27 10:13:42 +0700

    Web : Table - add tanstack, get table data using useQuery (hardcode)

--- web/components/map/Management/Table.vue
+++ web/components/map/Management/Table.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query";
 import IcCross from "~/assets/icons/ic-cross.svg";
 import IcDownload from "~/assets/icons/ic-download.svg";
 import IcExpand from "~/assets/icons/ic-expand.svg";
@@ -8,187 +9,193 @@ import IcSort from "~/assets/icons/ic-sort.svg";
 const store = useTableData();
 const { toggleTable, toggleFullscreen } = store;
 
-const columns = [
-  {
-    key: "name",
-    label: "Name",
-  },
-  {
-    key: "title",
-    label: "Title",
-  },
-  {
-    key: "email",
-    label: "Email",
-  },
-  {
-    key: "role",
-    label: "Role",
-  },
-  // {
-  //   key: "actions",
-  // },
-];
-
-const dummyData = [
-  {
-    id: 1,
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  {
-    id: 2,
-    name: "Courtney Henry",
-    title: "Designer",
-    email: "courtney.henry@example.com",
-    role: "Admin",
-  },
-  {
-    id: 3,
-    name: "Tom Cook",
-    title: "Director of Product",
-    email: "tom.cook@example.com",
-    role: "Member",
-  },
-  {
-    id: 4,
-    name: "Whitney Francis",
-    title: "Copywriter",
-    email: "whitney.francis@example.com",
-    role: "Admin",
-  },
-  {
-    id: 5,
-    name: "Leonard Krasner",
-    title: "Senior Designer",
-    email: "leonard.krasner@example.com",
-    role: "Owner",
-  },
-  {
-    id: 6,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 7,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 8,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 9,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 10,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 11,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 12,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 13,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 14,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 15,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 16,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-  {
-    id: 17,
-    name: "Floyd Miles",
-    title: "Principal Designer",
-    email: "floyd.miles@example.com",
-    role: "Member",
-  },
-];
-
-// const items = (row: any) => [
-//   [
-//     {
-//       label: "Edit",
-//       icon: "i-heroicons-pencil-square-20-solid",
-//       click: () => console.log("Edit", row.id),
-//     },
-//     {
-//       label: "Duplicate",
-//       icon: "i-heroicons-document-duplicate-20-solid",
-//     },
-//   ],
-//   [
-//     {
-//       label: "Archive",
-//       icon: "i-heroicons-archive-box-20-solid",
-//     },
-//     {
-//       label: "Move",
-//       icon: "i-heroicons-arrow-right-circle-20-solid",
-//     },
-//   ],
-//   [
-//     {
-//       label: "Delete",
-//       icon: "i-heroicons-trash-20-solid",
-//     },
-//   ],
+// const columns = [
+//   {
+//     key: "name",
+//     label: "Name",
+//   },
+//   {
+//     key: "address",
+//     label: "address",
+//   },
+//   // {
+//   //   key: "email",
+//   //   label: "Email",
+//   // },
+//   // {
+//   //   key: "role",
+//   //   label: "Role",
+//   // },
+//   // {
+//   //   key: "actions",
+//   // },
 // ];
 
-const selected = ref([dummyData[1]]);
+// const dummyData = [
+//   {
+//     id: 1,
+//     name: "Lindsay Walton",
+//     title: "Front-end Developer",
+//     email: "lindsay.walton@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 2,
+//     name: "Courtney Henry",
+//     title: "Designer",
+//     email: "courtney.henry@example.com",
+//     role: "Admin",
+//   },
+//   {
+//     id: 3,
+//     name: "Tom Cook",
+//     title: "Director of Product",
+//     email: "tom.cook@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 4,
+//     name: "Whitney Francis",
+//     title: "Copywriter",
+//     email: "whitney.francis@example.com",
+//     role: "Admin",
+//   },
+//   {
+//     id: 5,
+//     name: "Leonard Krasner",
+//     title: "Senior Designer",
+//     email: "leonard.krasner@example.com",
+//     role: "Owner",
+//   },
+//   {
+//     id: 6,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 7,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 8,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 9,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 10,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 11,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 12,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 13,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 14,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 15,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 16,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+//   {
+//     id: 17,
+//     name: "Floyd Miles",
+//     title: "Principal Designer",
+//     email: "floyd.miles@example.com",
+//     role: "Member",
+//   },
+// ];
+
+const selected = ref([]);
 
 const page = ref(1);
-const pageCount = 15;
+const pageCount = ref(10);
 
-const rows = computed(() => {
-  return dummyData.slice((page.value - 1) * pageCount, page.value * pageCount);
+// const rows = computed(() => {
+//   return dummyData.slice((page.value - 1) * pageCount.value, page.value * pageCount.value);
+// });
+
+/////
+const columns = ref([
+  {
+    key: "nama_jalan",
+    label: "Nama Jalan",
+  },
+  {
+    key: "fungsi",
+    label: "Fungsi",
+  },
+]);
+
+const fetcher = async (url: string, searchParams?: any): Promise<any> =>
+  await fetch(`${url}${searchParams ? "?" + searchParams : ""}`).then(
+    (response) => response.json()
+  );
+
+const { isLoading, isError, isFetching, data, error, refetch } = useQuery({
+  queryKey: ["/panel/items/jaringan_jalan_bandung"],
+  queryFn: ({ queryKey }) =>
+    fetcher(
+      queryKey[0],
+      new URLSearchParams({
+        limit: pageCount.value.toString(),
+        offset: ((page.value - 1) * pageCount.value).toString(),
+        meta: "filter_count",
+      })
+    ),
+});
+
+watchEffect(() => {
+  if (page.value) {
+    refetch();
+  }
 });
 </script>
 
@@ -217,7 +224,7 @@ const rows = computed(() => {
             class="w-[14px] h-[14px] text-grey-400"
             :fontControlled="false"
           />
-          Data Layer Pemetaan Kota
+          Data Jaringan Jalan Bandung
         </button>
         <div class="border-l border-grey-700 h-8"></div>
         <button
@@ -260,7 +267,7 @@ const rows = computed(() => {
     <div class="h-[calc(100%-5.5rem)] flex flex-col gap-2">
       <UTable
         v-model="selected"
-        :rows="rows"
+        :rows="data?.data"
         :columns="columns"
         class="overflow-scroll"
         :ui="{
@@ -283,7 +290,7 @@ const rows = computed(() => {
           },
         }"
       >
-        <template #name-data="{ row }">
+        <!-- <template #name-data="{ row }">
           <span
             :class="[
               selected.find((person) => person.id === row.id) &&
@@ -291,7 +298,7 @@ const rows = computed(() => {
             ]"
             >{{ row.name }}</span
           >
-        </template>
+        </template> -->
 
         <!-- <template #actions-data="{ row }">
           <UDropdown :items="items(row)">
@@ -307,7 +314,7 @@ const rows = computed(() => {
         <UPagination
           v-model="page"
           :page-count="pageCount"
-          :total="dummyData.length"
+          :total="data?.meta.filter_count"
           :active-button="{ variant: 'paginationActive' }"
           :inactive-button="{ variant: 'paginationInactive' }"
           :prev-button="{ variant: 'paginationInactive' }"
