3905cb8a64b0f3e24b8b13a80cc1a325b7c9c2bd
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-02-28 23:52:05 +0700

    Extensions: Add visible, opacity, point_size, point_color columns into three-d-tiles-structure migration

--- extensions/extensions/migrations/20231204GD7-three-d-tiles-structure.mjs
+++ extensions/extensions/migrations/20231204GD7-three-d-tiles-structure.mjs
@@ -10,7 +10,11 @@ export async function up(knex) {
     layer_alias character varying(255) NOT NULL,
     listed boolean DEFAULT false NOT NULL,
     active boolean DEFAULT false NOT NULL,
-    permission_type character varying(255) DEFAULT 'admin'
+    permission_type character varying(255) DEFAULT 'admin',
+    visible boolean DEFAULT true NOT NULL,
+    opacity real,
+    point_size real,
+    point_color character varying(255)
   );
 
     INSERT INTO directus_collections(collection,icon,color,"group",collapse)
@@ -40,6 +44,10 @@ export async function up(knex) {
       ('three_d_tiles','active','cast-boolean','boolean','{"label":"True"}',NULL,NULL,FALSE,FALSE,NULL,'half',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
       ('three_d_tiles','permission_type',NULL,'select-dropdown','{"choices":[{"text":"Admin Only","value":"admin"},{"text":"Selected Roles","value":"roles"},{"text":"Selected Roles + Public","value":"roles+public"}]}',NULL,NULL,FALSE,FALSE,NULL,'half',NULL,NULL,NULL,TRUE,NULL,NULL,NULL),
       ('three_d_tiles','allowed_roles','m2m','list-m2m','{"enableCreate":false,"filter":{"_and":[{"admin_access":{"_eq":false}}]},"template":"{{directus_roles_id.name}}"}',NULL,NULL,FALSE,FALSE,NULL,'half',NULL,NULL,'[{"name":"Hide if permission type is admin","rule":{"_and":[{"permission_type":{"_eq":"admin"}}]},"hidden":true,"options":{"layout":"list","enableCreate":false,"enableSelect":true,"limit":15,"junctionFieldLocation":"bottom","allowDuplicates":false,"enableSearchFilter":false,"enableLink":false}},{"name":"Require when permission type is roles","rule":{"_and":[{"permission_type":{"_eq":"roles"}}]},"required":true,"options":{"layout":"list","enableCreate":false,"enableSelect":true,"limit":15,"junctionFieldLocation":"bottom","allowDuplicates":false,"enableSearchFilter":false,"enableLink":false}}]',FALSE,NULL,NULL,NULL),('three_d_tiles_directus_roles','id',NULL,NULL,NULL,NULL,NULL,FALSE,TRUE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('three_d_tiles','visible','cast-boolean','boolean','{"label":"True"}',NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,TRUE,NULL,NULL,NULL),
+      ('three_d_tiles','point_color',NULL,'select-color','{"opacity":true}',NULL,NULL,FALSE,FALSE,NULL,'full',NULL,'Only applied on point features that have no color attribute',NULL,FALSE,NULL,NULL,NULL),
+      ('three_d_tiles','point_size',NULL,'slider','{"minValue":0.1,"stepInterval":0.1,"alwaysShowValue":true}',NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
+      ('three_d_tiles','opacity',NULL,'slider','{"minValue":0,"maxValue":1,"stepInterval":0.1,"alwaysShowValue":true}',NULL,NULL,FALSE,FALSE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
       ('three_d_tiles_directus_roles','three_d_tiles_layer_id',NULL,NULL,NULL,NULL,NULL,FALSE,TRUE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL),
       ('three_d_tiles_directus_roles','directus_roles_id',NULL,NULL,NULL,NULL,NULL,FALSE,TRUE,NULL,'full',NULL,NULL,NULL,FALSE,NULL,NULL,NULL);
 
