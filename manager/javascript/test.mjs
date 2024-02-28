a78398f8b3a35a09f646b7c8dcf042e956c4bb82
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-02-29 00:27:22 +0700

    Manager: Make all files uniform as mjs

---
+++ manager/javascript/test.mjs
@@ -0,0 +1,17 @@
+import { quickAddJob } from "graphile-worker";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function enqueueGenerateSprites() {
+  await quickAddJob(
+    { connectionString: process.env.DB_CONNECTION_STRING },
+    "generateSprites",
+    { name: "Bobby Tables" }
+  );
+}
+
+const test = async () => {
+  enqueueGenerateSprites();
+};
+test();
