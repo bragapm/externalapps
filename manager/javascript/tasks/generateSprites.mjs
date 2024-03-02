31f7e7f917d5da2382634987357c54391f724c35
Miko <jherjati@gmail.com>
2024-03-02 16:37:47 +0700

    Graphile : swap to spreet rust

--- manager/javascript/tasks/generateSprites.mjs
+++ manager/javascript/tasks/generateSprites.mjs
@@ -1,31 +1,10 @@
 import fs from "fs";
 import os from "os";
 import path from "path";
-import spritezero from "@mapbox/spritezero";
+import { execa } from "execa";
 
 import { LAYER_ICONS_FOLDER_ID } from "../utils/const.mjs";
 import minioClient from "../utils/minioClient.mjs";
-
-function generateLayoutAsync(svgs, pixelRatio, format, sdf) {
-  return new Promise((resolve, reject) => {
-    spritezero.generateLayout(
-      { imgs: svgs, pixelRatio, format, sdf },
-      (err, layout) => {
-        if (err) reject(err);
-        else resolve(layout);
-      }
-    );
-  });
-}
-
-function generateImageAsync(layout) {
-  return new Promise((resolve, reject) => {
-    spritezero.generateImage(layout, (err, image) => {
-      if (err) reject(err);
-      else resolve(image);
-    });
-  });
-}
 
 export default async function ({ queueId }, helpers) {
   const bucketName = process.env.STORAGE_S3_BUCKET;
@@ -66,28 +45,23 @@ export default async function ({ queueId }, helpers) {
     );
 
     for (const pxRatio of [1, 2, 4]) {
-      const svgs = iconKeys.map((key) => ({
-        svg: fs.readFileSync(path.join(spritesDir, key)),
-        id: path.basename(key, ".svg"),
-      }));
-
-      const pngPath = path.resolve(spritesDir, `sprite@${pxRatio}x.png`);
-      const jsonPath = path.resolve(spritesDir, `sprite@${pxRatio}x.json`);
-
-      // Generate image layout for PNG sprite image
-      const imageLayout = await generateLayoutAsync(svgs, pxRatio, false, true);
-      const image = await generateImageAsync(imageLayout);
-      fs.writeFileSync(pngPath, image);
-
-      // Generate data layout for JSON sprite manifest
-      const dataLayout = await generateLayoutAsync(svgs, pxRatio, true, true);
-      fs.writeFileSync(jsonPath, JSON.stringify(dataLayout));
+      const filePath = path.resolve(spritesDir, `sprite@${pxRatio}x`);
+      const args = [
+        "--ratio",
+        pxRatio,
+        "--unique",
+        "--minify-index-file",
+        "--sdf",
+        spritesDir,
+        filePath,
+      ];
+      await execa(`spreet`, args);
 
       // Upload JSON and PNG to MinIO
       await minioClient.fPutObject(
         bucketName,
         `sprites/sprite@${pxRatio}x.png`,
-        pngPath,
+        filePath + ".png",
         { "Content-Type": "image/png" }
       );
       console.log(`Upload PNG ${pxRatio} Success`);
@@ -95,7 +69,7 @@ export default async function ({ queueId }, helpers) {
       await minioClient.fPutObject(
         bucketName,
         `sprites/sprite@${pxRatio}x.json`,
-        jsonPath,
+        filePath + ".json",
         { "Content-Type": "application/json" }
       );
       console.log(`Upload JSON ${pxRatio} Success`);
