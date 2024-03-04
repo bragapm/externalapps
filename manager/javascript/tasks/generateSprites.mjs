425095c3e67760bce3171d94334c2abd8714cc0e
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-03-04 20:15:46 +0700

    Graphile: Minor tidy up in import statements

--- manager/javascript/tasks/generateSprites.mjs
+++ manager/javascript/tasks/generateSprites.mjs
@@ -1,6 +1,7 @@
-import fs from "fs";
-import os from "os";
-import path from "path";
+import fs from "node:fs";
+import os from "node:os";
+import path from "node:path";
+
 import { execa } from "execa";
 
 import { LAYER_ICONS_FOLDER_ID } from "../utils/const.mjs";
