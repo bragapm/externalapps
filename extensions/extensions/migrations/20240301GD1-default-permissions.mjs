9e39c10a34c9ba4363e70677b308470329900829
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-04-18 14:58:45 +0700

    Migrations: Allow registered user to upload to layer previews folder

--- extensions/extensions/migrations/20240301GD1-default-permissions.mjs
+++ extensions/extensions/migrations/20240301GD1-default-permissions.mjs
@@ -1,4 +1,8 @@
-import { PUBLIC_FOLDER_ID, LAYER_DATA_FOLDER_ID } from "./const/FOLDER_IDS.mjs";
+import {
+  PUBLIC_FOLDER_ID,
+  LAYER_DATA_FOLDER_ID,
+  LAYER_PREVIEWS_FOLDER_ID,
+} from "./const/FOLDER_IDS.mjs";
 
 export async function up(knex) {
   await knex.raw(`
@@ -58,7 +62,7 @@ export async function up(knex) {
           (NEW.id,'categories','read','{}','{}','*'),
           (NEW.id,'three_d_tiles','read','{"_and":[{"permission_type":{"_in":["roles","roles+public"]}},{"allowed_roles":{"directus_roles_id":{"_eq":"$CURRENT_ROLE"}}}]}','{}','layer_id,layer_alias,category,active,visible,opacity,point_size,point_color'),
           (NEW.id,'shared_map','create','{}','{}','*'),
-          (NEW.id,'directus_files','create','{}','{"_and":[{"folder":{"_eq":"${LAYER_DATA_FOLDER_ID}"}}]}','*'),
+          (NEW.id,'directus_files','create','{}','{"_and":[{"folder":{"_in":["${LAYER_DATA_FOLDER_ID}","${LAYER_PREVIEWS_FOLDER_ID}"]}}]}','*'),
           (NEW.id,'geoprocessing_queue','read','{"_and":[{"uploader":{"_eq":"$CURRENT_USER"}}]}','{}','result,state,filename,status,mtime,result_ttl');
         RETURN NULL;
       END;
