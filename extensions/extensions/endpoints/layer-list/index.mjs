3c9c840e2e144d956e6ca0df103cc7e1e73258a6
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-04-24 12:42:31 +0700

    Extensions: Add search query in layer list endpoint

--- extensions/extensions/endpoints/layer-list/index.mjs
+++ extensions/extensions/endpoints/layer-list/index.mjs
@@ -9,6 +9,7 @@ export default (router, { database, logger }) => {
       sort_dir: sortDir = "asc",
       dimension = "all",
       format = "all",
+      search,
     } = req.query;
     const formatArr = format.split(",");
 
@@ -60,6 +61,9 @@ export default (router, { database, logger }) => {
       ["all", "3d"].includes(dimension) &&
       ["all", "terrain"].some((el) => formatArr.includes(el));
 
+    const paramBindings = {};
+    if (search) paramBindings.search = `%${search}%`;
+
     try {
       if (groupBy === "category") {
         const query = allByCategoriesQuery(
@@ -69,10 +73,11 @@ export default (router, { database, logger }) => {
           fetch2d,
           fetch3d,
           fetchTerrain,
-          formatArr
+          formatArr,
+          search
         );
         if (query) {
-          const result = await database.raw(query);
+          const result = await database.raw(query, paramBindings);
           return res.json({ data: result.rows[0]?.all_by_categories || {} });
         } else {
           return res.json({ data: {} });
@@ -80,13 +85,22 @@ export default (router, { database, logger }) => {
       } else {
         const [threeDResult, twoDResult, terrainResult] = await Promise.all([
           fetch3d
-            ? database.raw(threeDQuery(state, accountability, sortDir))
+            ? database.raw(
+                threeDQuery(state, accountability, sortDir, search),
+                paramBindings
+              )
             : false,
           fetch2d
-            ? database.raw(twoDQuery(state, accountability, sortDir, formatArr))
+            ? database.raw(
+                twoDQuery(state, accountability, sortDir, formatArr, search),
+                paramBindings
+              )
             : false,
           fetchTerrain
-            ? database.raw(terrainQuery(state, accountability, sortDir))
+            ? database.raw(
+                terrainQuery(state, accountability, sortDir, search),
+                paramBindings
+              )
             : false,
         ]);
         const data = {};
@@ -108,7 +122,7 @@ export default (router, { database, logger }) => {
   });
 };
 
-function threeDQuery(state, accountability, sortDir) {
+function threeDQuery(state, accountability, sortDir, search) {
   let allowedRoleJoin = "";
   let permissionFilter = "";
   if (accountability.admin) {
@@ -118,6 +132,7 @@ function threeDQuery(state, accountability, sortDir) {
   } else {
     permissionFilter = "AND permission_type = 'roles+public'";
   }
+  const searchFilter = search ? "AND layer_alias ILIKE :search" : "";
 
   return `WITH three_d_tiles_list AS (
     SELECT layer_id,layer_alias,preview,description,category,opacity,point_size,point_color,visible
@@ -125,6 +140,7 @@ function threeDQuery(state, accountability, sortDir) {
     ${allowedRoleJoin}
     WHERE ${state} IS TRUE
     ${permissionFilter}
+    ${searchFilter}
   ), category_3d AS (
     SELECT category,to_jsonb(l2) layers
     FROM (
@@ -149,7 +165,7 @@ function threeDQuery(state, accountability, sortDir) {
   ) cg`;
 }
 
-function twoDQuery(state, accountability, sortDir, formatArr) {
+function twoDQuery(state, accountability, sortDir, formatArr, search) {
   let allowedRoleVectorJoin = "";
   let allowedRoleRasterJoin = "";
   let permissionFilter = "";
@@ -161,6 +177,10 @@ function twoDQuery(state, accountability, sortDir, formatArr) {
   } else {
     permissionFilter = "AND permission_type = 'roles+public'";
   }
+  const vectorSearchFilter = search
+    ? "AND COALESCE(layer_alias,layer_name) ILIKE :search"
+    : "";
+  const rasterSearchFilter = search ? "AND layer_alias ILIKE :search" : "";
 
   const listQueries = [];
   const unionQueries = [];
@@ -196,6 +216,7 @@ function twoDQuery(state, accountability, sortDir, formatArr) {
       ${allowedRoleVectorJoin}
       WHERE ${state} IS TRUE
       ${permissionFilter}
+      ${vectorSearchFilter}
     ), layer_styles AS (
       ${styleUnionQueries.join(" UNION ALL ")}
     ), category_vector AS (
@@ -219,6 +240,7 @@ function twoDQuery(state, accountability, sortDir, formatArr) {
       WHERE ${state} IS TRUE
       AND terrain_rgb IS FALSE
       ${permissionFilter}
+      ${rasterSearchFilter}
     ), category_raster AS (
       SELECT category,to_jsonb(l2) layers
       FROM (
@@ -250,7 +272,7 @@ function twoDQuery(state, accountability, sortDir, formatArr) {
   ) cg`;
 }
 
-function terrainQuery(state, accountability, sortDir) {
+function terrainQuery(state, accountability, sortDir, search) {
   let allowedRoleJoin = "";
   let permissionFilter = "";
   if (accountability.admin) {
@@ -260,6 +282,7 @@ function terrainQuery(state, accountability, sortDir) {
   } else {
     permissionFilter = "AND permission_type = 'roles+public'";
   }
+  const searchFilter = search ? "AND layer_alias ILIKE :search" : "";
 
   return `WITH terrain_list AS (
     SELECT layer_id,bounds,minzoom,maxzoom,layer_alias,preview,description,category,visible
@@ -268,6 +291,7 @@ function terrainQuery(state, accountability, sortDir) {
     WHERE ${state} IS TRUE
     AND terrain_rgb IS TRUE
     ${permissionFilter}
+    ${searchFilter}
   ), category_terrain AS (
     SELECT category,to_jsonb(l2) layers
     FROM (
@@ -299,7 +323,8 @@ function allByCategoriesQuery(
   fetch2d,
   fetch3d,
   fetchTerrain,
-  formatArr
+  formatArr,
+  search
 ) {
   let allowedRole3dJoin = "";
   let allowedRoleVectorJoin = "";
@@ -314,6 +339,10 @@ function allByCategoriesQuery(
   } else {
     permissionFilter = "AND permission_type = 'roles+public'";
   }
+  const vectorSearchFilter = search
+    ? "AND COALESCE(layer_alias,layer_name) ILIKE :search"
+    : "";
+  const searchFilter = search ? "AND layer_alias ILIKE :search" : "";
 
   const listQueries = [];
   const unionQueries = [];
@@ -350,6 +379,7 @@ function allByCategoriesQuery(
       ${allowedRoleVectorJoin}
       WHERE ${state} IS TRUE
       ${permissionFilter}
+      ${vectorSearchFilter}
     ), layer_styles AS (
       ${styleUnionQueries.join(" UNION ALL ")}
     ), category_vector AS (
@@ -372,6 +402,7 @@ function allByCategoriesQuery(
       ${allowedRole3dJoin}
       WHERE ${state} IS TRUE
       ${permissionFilter}
+      ${searchFilter}
     ), category_3d AS (
       SELECT category,to_jsonb(l2) layers
       FROM (
@@ -401,6 +432,7 @@ function allByCategoriesQuery(
       WHERE ${state} IS TRUE
       ${permissionFilter}
       ${rasterListFilter}
+      ${searchFilter}
     ), category_raster AS (
       SELECT category,to_jsonb(l2) layers
       FROM (
