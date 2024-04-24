6c7b98c5aec7857f86547a5810fe9aaa7b0db3da
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-04-24 12:21:53 +0700

    Extensions: Use parameter bindings for buffer queries

--- extensions/extensions/endpoints/buffer/index.mjs
+++ extensions/extensions/endpoints/buffer/index.mjs
@@ -5,13 +5,22 @@ export default (router, { database, logger }) => {
     const { points, radius, layer, type, column } = req.body;
 
     // Construct the array part of the SQL for points
-    const pointParams = points
-      .map((point) => `ST_MakePoint(${point[0]}, ${point[1]})`)
-      .join(", ");
+    let p = 0;
+    const pointBindings = {};
+    const makePointQueries = [];
+    for (const point of points) {
+      const bindingName0 = `point${p++}`;
+      const bindingName1 = `point${p++}`;
+      pointBindings[bindingName0] = point[0];
+      pointBindings[bindingName1] = point[1];
+      makePointQueries.push(`ST_MakePoint(:${bindingName0},:${bindingName1})`);
+    }
 
     const baseSQL = `
         WITH buffer AS (
-            SELECT ST_Buffer(ST_SetSRID(ST_Collect(ARRAY[${pointParams}]), 4326)::geography, ${radius})::geometry AS geom
+            SELECT ST_Buffer(ST_SetSRID(ST_Collect(ARRAY[${makePointQueries.join(
+              ","
+            )}]), 4326)::geography, :radius)::geometry AS geom
         )
     `;
 
@@ -19,19 +28,21 @@ export default (router, { database, logger }) => {
     let sql;
     if (type === "simple") {
       sql = `${baseSQL}
-               SELECT 'All' as category, COUNT(*) FROM "${layer}", buffer
-               WHERE ST_Intersects("${layer}".geom, buffer.geom);
+               SELECT 'All' as category, COUNT(*) FROM :layer:, buffer
+               WHERE ST_Intersects(:layer:.geom, buffer.geom);
         `;
     } else if (type === "categorical") {
       sql = `${baseSQL}
-               SELECT "${column}" as category, COUNT(*) FROM "${layer}", buffer
-               WHERE ST_Intersects("${layer}".geom, buffer.geom)
-               GROUP BY "${column}";
+               SELECT :column: as category, COUNT(*) FROM :layer:, buffer
+               WHERE ST_Intersects(:layer:.geom, buffer.geom)
+               GROUP BY :column:;
         `;
     }
     let result;
     try {
-      result = (await database.raw(sql)).rows;
+      result = (
+        await database.raw(sql, { ...pointBindings, radius, layer, column })
+      ).rows;
     } catch (error) {
       if (error.code === "FORBIDDEN") {
         return next(error);
