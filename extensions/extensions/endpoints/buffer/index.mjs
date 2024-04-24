fc7a5fa155c88afd11b56041ec7cfb00cdc4bfa1
khaidirsh <52772412+khaidirsh@users.noreply.github.com>
2024-04-24 13:53:25 +0700

    Extensions: Add payload checking in buffer endpoint

--- extensions/extensions/endpoints/buffer/index.mjs
+++ extensions/extensions/endpoints/buffer/index.mjs
@@ -1,14 +1,78 @@
-import { InvalidPayloadError, ServiceUnavailableError } from "@directus/errors";
+import {
+  InvalidPayloadError,
+  ServiceUnavailableError,
+  ForbiddenError,
+} from "@directus/errors";
 
 export default (router, { database, logger }) => {
   router.patch("/", async (req, res, next) => {
+    const { accountability } = req;
     const { points, radius, layer, type, column } = req.body;
+
+    if (!layer) {
+      return next(new InvalidPayloadError({ reason: "layer is required" }));
+    }
+
+    try {
+      const {
+        rows: [{ exists }],
+      } = await database.raw(
+        "SELECT EXISTS(SELECT 1 FROM vector_tiles WHERE layer_name = ?)",
+        layer
+      );
+      if (!exists) {
+        return next(new ForbiddenError());
+      }
+    } catch (error) {
+      logger.error(error);
+      return next(
+        new ServiceUnavailableError({
+          service: "buffer-analysis",
+          reason: "Failed to apply buffer analysis",
+        })
+      );
+    }
+
+    if (
+      !accountability.admin &&
+      !accountability.permissions.some(
+        (el) => el.collection === layer && el.action === "read"
+      )
+    ) {
+      return next(new ForbiddenError());
+    }
+
+    if (!Array.isArray(points)) {
+      return next(
+        new InvalidPayloadError({
+          reason: "points must be array of [lon,lat]",
+        })
+      );
+    }
+
+    if (
+      typeof radius !== "number" ||
+      !isFinite(radius) ||
+      isNaN(radius) ||
+      radius < 0
+    ) {
+      return next(
+        new InvalidPayloadError({ reason: "radius must be positive number" })
+      );
+    }
 
     // Construct the array part of the SQL for points
     let p = 0;
     const pointBindings = {};
     const makePointQueries = [];
     for (const point of points) {
+      if (!Array.isArray(point) || point.length !== 2) {
+        return next(
+          new InvalidPayloadError({
+            reason: "points must be array of [lon,lat]",
+          })
+        );
+      }
       const bindingName0 = `point${p++}`;
       const bindingName1 = `point${p++}`;
       pointBindings[bindingName0] = point[0];
@@ -32,6 +96,11 @@ export default (router, { database, logger }) => {
                WHERE ST_Intersects(:layer:.geom, buffer.geom);
         `;
     } else if (type === "categorical") {
+      if (!column) {
+        return next(new InvalidPayloadError({ reason: "column is required" }));
+      } else if (!req.schema.collections[layer]?.fields[column]) {
+        return next(new ForbiddenError());
+      }
       sql = `${baseSQL}
                SELECT :column: as category, COUNT(*) FROM :layer:, buffer
                WHERE ST_Intersects(:layer:.geom, buffer.geom)
