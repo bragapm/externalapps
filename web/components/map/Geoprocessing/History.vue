4f9c460490079c42c3292d92a7d19fdcc8dfd3b8
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2024-10-03 23:45:28 +0700

    Geoprocessing Queue: get queue history using useInfiniteQuery, handle infinite scroll, add skeleton load

--- web/components/map/Geoprocessing/History.vue
+++ web/components/map/Geoprocessing/History.vue
@@ -1,44 +1,99 @@
 <script lang="ts" setup>
-import { useQuery } from "@tanstack/vue-query";
+import { useInfiniteQuery } from "@tanstack/vue-query";
+import IcSpinner from "~/assets/icons/ic-spinner.svg";
 
 const authStore = useAuth();
 
 const {
-  data: historyData,
-} = useQuery({
+  data: historyDataInfinite,
+  fetchNextPage,
+  hasNextPage,
+  isFetching,
+} = useInfiniteQuery({
   queryKey: ["geoprocessing_history_query_key"],
-  queryFn: async () => {
-    const res = await $fetch<{ data: any }>(
-      "/panel/items/geoprocessing_queue?" +
-        new URLSearchParams({
-          fields: "*,uploader.*",
-          sort: "-mtime",
-          "filter[state][_eq]": "done",
-        }),
+  queryFn: async ({ pageParam = 1, queryKey }) => {
+    const queryParams: Record<string, string> = {
+      limit: "10",
+      page: pageParam.toString(),
+      fields: "*,uploader.*",
+      sort: "-mtime",
+      "filter[state][_eq]": "done",
+    };
+    const r = await $fetch<{ data: any[] }>(
+      "/panel/items/geoprocessing_queue?" + new URLSearchParams(queryParams),
       {
         headers: { Authorization: "Bearer " + authStore.accessToken },
       }
     );
-    return res.data;
+    return r.data;
+  },
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) => {
+    if (lastPage.length < 10) {
+      return undefined;
+    }
+    return lastPageParam + 1;
   },
   refetchInterval: 15 * 1000,
+});
+
+const sentinel = ref(null);
+const { isVisible } = useIntersectionObserver(sentinel);
+
+watchEffect(() => {
+  if (isVisible.value && hasNextPage.value) {
+    fetchNextPage();
+  }
 });
 </script>
 
 <template>
   <div class="space-y-2">
     <div
-      v-if="historyData"
-      v-for="(item, index) of historyData"
+      v-if="historyDataInfinite?.pages.flat().length"
+      v-for="(item, index) of historyDataInfinite.pages.flat()"
       :key="item.message_id"
     >
-      <MapGeoprocessingCard
-        :data="item"
-        :isAddLayerAction="true"
-      />
+      <MapGeoprocessingCard :data="item" :isAddLayerAction="true" />
     </div>
-    <div v-else class="text-white text-center mt-5">
+    <div
+      v-if="!historyDataInfinite?.pages.flat().length && !isFetching"
+      class="text-white text-center mt-5"
+    >
       <h4 class="text-sm text-grey-50">No Data</h4>
+    </div>
+    <div
+      v-if="!historyDataInfinite?.pages.flat().length && isFetching"
+      v-for="i of [1, 2, 3]"
+      :key="i"
+    >
+      <USkeleton
+        :ui="{ background: 'bg-grey-800', rounded: 'rounded-xxs' }"
+        class="h-10 w-full"
+      />
+      <div
+        class="bg-transparent border border-grey-700 rounded-xxs p-2 space-y-2"
+      >
+        <div v-for="i of [1, 2, 3, 4]" :key="i" class="space-y-1">
+          <USkeleton
+            :ui="{ background: 'bg-grey-800', rounded: 'rounded-xxs' }"
+            class="h-3 w-1/2"
+          />
+          <USkeleton
+            :ui="{ background: 'bg-grey-800', rounded: 'rounded-xxs' }"
+            class="h-3 w-2/3"
+          />
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="historyDataInfinite?.pages.flat().length && hasNextPage"
+      ref="sentinel"
+    >
+      <IcSpinner
+        class="text-white animate-spin h-6 w-6 p-1 m-auto"
+        :fontControlled="false"
+      />
     </div>
   </div>
 </template>
