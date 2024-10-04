cdf58aba6225aa26283170f21220696a42b00de6
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2024-10-04 11:16:26 +0700

    Geoprocessing Queue: get queue list using useInfiniteQuery, handle infinite scroll, add skeleton load

--- web/components/map/Geoprocessing/Queue.vue
+++ web/components/map/Geoprocessing/Queue.vue
@@ -1,121 +1,101 @@
 <script lang="ts" setup>
-import type { Queue } from "~/utils/types";
+import { useInfiniteQuery } from "@tanstack/vue-query";
+import IcSpinner from "~/assets/icons/ic-spinner.svg";
 
 const authStore = useAuth();
-const toast = useToast();
-
-function detectStateChanges(prevState: Queue[], nextState: Queue[]): Queue[] {
-  const prevStateMap = new Map<string, Queue>();
-  const result: Queue[] = [];
-
-  // Create a map from prevState for quick lookup
-  for (const obj of prevState) {
-    prevStateMap.set(obj.message_id, obj);
-  }
-
-  // Iterate over nextState to find objects that changed to "done"
-  for (const obj of nextState) {
-    const prevObj = prevStateMap.get(obj.message_id);
-
-    if (["done", "rejected"].includes(obj.state)) {
-      if (!prevObj || !["done", "rejected"].includes(prevObj.state)) {
-        result.push(obj);
+const pageLimit = 10;
+const {
+  data: queueDataInfinite,
+  fetchNextPage,
+  hasNextPage,
+  isFetching,
+} = useInfiniteQuery({
+  queryKey: ["geoprocessing_queue_query_key"],
+  queryFn: async ({ pageParam = 1, queryKey }) => {
+    const queryParams: Record<string, string> = {
+      limit: pageLimit.toString(),
+      page: pageParam.toString(),
+      fields: "*,uploader.*",
+      sort: "-mtime",
+      filter: JSON.stringify({
+        _or: [{ state: { _eq: "consumed" } }, { state: { _eq: "queued" } }],
+      }),
+    };
+    const r = await $fetch<{ data: any[] }>(
+      "/panel/items/geoprocessing_queue?" + new URLSearchParams(queryParams),
+      {
+        headers: { Authorization: "Bearer " + authStore.accessToken },
       }
+    );
+    return r.data;
+  },
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) => {
+    if (lastPage.length < pageLimit) {
+      return undefined;
     }
+    return lastPageParam + 1;
+  },
+  refetchInterval: 15 * 1000,
+});
+
+const sentinel = ref(null);
+const { isVisible } = useIntersectionObserver(sentinel);
+
+watchEffect(() => {
+  if (isVisible.value && hasNextPage.value) {
+    fetchNextPage();
   }
-
-  return result;
-}
-const queue = ref<Queue[]>([]);
-
-watchEffect((onInvalidate) => {
-  if (!authStore.accessToken) return;
-
-  const fetchQueueList = async () => {
-    try {
-      const response = await $fetch<{ data: any }>(
-        "/panel/items/geoprocessing_queue?" +
-          new URLSearchParams({
-            fields: "*,uploader.*",
-            sort: "-mtime",
-            "filter[state][_neq]": "done",
-          }),
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${authStore.accessToken}`,
-            "Cache-Control": "no-store",
-          },
-        }
-      );
-      const result = await response.data;
-
-      if (queue.value.length) {
-        const changedQueue = detectStateChanges(
-          queue.value,
-          result.filter((item: any) =>
-            ["intersect"].includes(item.message.actor_name)
-          )
-        );
-        changedQueue.forEach((item) => {
-          if (item.state === "rejected") {
-            toast.add({
-              title: `Your ${item.message.actor_name} task has been rejected. Please review and try again.`,
-              icon: "i-heroicons-x-mark",
-            });
-          } else {
-            if (item.status === "success") {
-              toast.add({
-                title: `Your ${item.message.actor_name} task has been successfully processed!`,
-                icon: "i-heroicons-check-circle",
-              });
-            } else if (item.status === "error") {
-              toast.add({
-                title: `There was an error processing your ${item.message.actor_name} task. Please try again.`,
-                icon: "i-heroicons-x-mark",
-              });
-            }
-          }
-        });
-      }
-
-      queue.value = result.filter((item: any) =>
-        ["intersect"].includes(item.message.actor_name)
-      );
-    } catch (error) {
-      const message = error instanceof Error ? error.message : "Error";
-      toast.add({
-        title: "Error",
-        description: message,
-      });
-    }
-  };
-
-  fetchQueueList();
-  const interval = setInterval(() => {
-    fetchQueueList();
-  }, 15000);
-
-  onInvalidate(() => {
-    clearInterval(interval);
-  });
 });
 </script>
 
 <template>
   <div class="space-y-2">
     <div
-      v-if="queue.length"
-      v-for="(item, index) of queue"
+      v-if="queueDataInfinite?.pages.flat().length"
+      v-for="(item, index) of queueDataInfinite.pages.flat()"
       :key="item.message_id"
     >
       <MapGeoprocessingCard :data="item" />
     </div>
     <div
-      v-else
+      v-if="!queueDataInfinite?.pages.flat().length && !isFetching"
       class="text-white text-center mt-5"
     >
       <h4 class="text-sm text-grey-50">No Data</h4>
+    </div>
+    <div
+      v-if="!queueDataInfinite?.pages.flat().length && isFetching"
+      v-for="i of [1, 2, 3]"
+      :key="i"
+    >
+      <USkeleton
+        :ui="{ background: 'bg-grey-800', rounded: 'rounded-xxs' }"
+        class="h-10 w-full"
+      />
+      <div
+        class="bg-transparent border border-grey-700 rounded-xxs p-2 space-y-2"
+      >
+        <div v-for="i of [1, 2, 3, 4]" :key="i" class="space-y-1">
+          <USkeleton
+            :ui="{ background: 'bg-grey-800', rounded: 'rounded-xxs' }"
+            class="h-3 w-1/2"
+          />
+          <USkeleton
+            :ui="{ background: 'bg-grey-800', rounded: 'rounded-xxs' }"
+            class="h-3 w-2/3"
+          />
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="queueDataInfinite?.pages.flat().length && hasNextPage"
+      ref="sentinel"
+    >
+      <IcSpinner
+        class="text-white animate-spin h-6 w-6 p-1 m-auto"
+        :fontControlled="false"
+      />
     </div>
   </div>
 </template>
