46f11504aa9326223c02929ebacf83e677445968
“bismowibi” <bismowibi@gmail.com>
2025-06-21 21:36:04 +0700

    Web : add bg color on page

--- web/components/layouts/dashboard/index.vue
+++ web/components/layouts/dashboard/index.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="h-screen flex flex-col justify-center overflow-hidden max-w-[90rem] mx-auto">
+  <div
+    class="h-screen flex flex-col justify-center overflow-hidden bg-[#E2E1DF]"
+  >
     <LayoutsDashboardNavbar />
     <div class="flex flex-1 overflow-hidden gap-3">
       <LayoutsDashboardSidebar />
